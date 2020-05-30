f748a3cab076a849971a70454bd9fa025f8c3165
igor <embiggenerd@gmail.com>
2020-05-30 16:59:00 -0400

    remote database connected

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
-const lala = "hihi"
+import { config } from 'dotenv'
+console.log(config())
 
-console.log(lala)
+const { Pool, Client } = require('pg')
+// pools will use environment variables
+// for connection information
+const initDB = async()=>{
+
+  const pool = new Pool()
+  pool.query('SELECT NOW()', (err: Error, res: any) => {
+    console.log(err, res)
+    pool.end()
+  })
+  
+  // you can also use async/await
+  // const res = await pool.query('SELECT NOW()')
+  // await pool.end()
+  // // clients will also use environment variables
+  // // for connection information
+  // const client = new Client()
+  // await client.connect()
+  // const res = await client.query('SELECT NOW()')
+  // await client.end()
+}
+
+initDB()
